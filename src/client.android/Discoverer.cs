cca4f8b2ea335c69967ec3516289a9621f30c539
Malachi Burke <malachi.burke@gmail.com>
2017-07-03 13:27:54 -0700

    Upgrading packages

---
+++ src/client.android/Discoverer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace client.android
+{
+    // Looks for open AP with a particular IP address
+    public class Discoverer
+    {
+        public Discoverer()
+        {
+
+        }
+    }
+}
