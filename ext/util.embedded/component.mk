c3b655558fef8553cbe0eaf71f1cee698b2d8f98
Malachi Burke <malachi.burke@gmail.com>
2017-07-08 12:03:07 -0700

    Adding util.embedded to project
    Needs esp-open-rtos support built out

---
+++ ext/util.embedded/component.mk
@@ -0,0 +1,11 @@
+INC_DIRS += $(util_embedded_ROOT)util.embedded/src
+
+util_embedded_SRC_DIR += $(util_embedded_ROOT)util.embedded/src
+util_embedded_SRC_DIR += $(util_embedded_ROOT)util.embedded/src/fact
+util_embedded_SRC_DIR += $(util_embedded_ROOT)util.embedded/src/fact/hardware
+#util_embedded_SRC_DIR += $(util_embedded_ROOT)util.embedded/src/driver/atmel/sam
+# OK don't try to pull in EVERYTHING just yet!!
+#sming_SRC_DIR += $(sming_ROOT)Sming/Sming/SmingCore
+util_embedded_CPPFLAGS += -DESP_OPEN_RTOS -DFEATURE_IOSTREAM_SHIM -std=c++11 -DNODUINO
+
+$(eval $(call component_compile_rules,util_embedded))
