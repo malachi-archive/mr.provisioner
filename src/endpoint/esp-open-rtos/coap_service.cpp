e2a7ab8ac6addca541e634fd542152978ec492fa
Malachi Burke <malachi.burke@gmail.com>
2017-07-13 20:37:52 -0700

    Commenting in prep for real wifi ssid/pass put

--- src/endpoint/esp-open-rtos/coap_service.cpp
+++ src/endpoint/esp-open-rtos/coap_service.cpp
@@ -25,6 +25,8 @@ static int handle_put_ssid_name(const coap_resource_t *resource,
                             const coap_packet_t *inpkt,
                             coap_packet_t *pkt);
 
+struct sdk_station_config config;
+
 coap_resource_t resources[] =
 {
     {COAP_RDY, COAP_METHOD_GET, COAP_TYPE_ACK,
@@ -67,6 +69,17 @@ static int handle_put_ssid_name(const coap_resource_t *resource,
         printf("Light OFF\n");
     }*/
     printf("ssid name = %s\n", inpkt->payload.p);
+
+    /* TODO: we'll do this once we get our hands on PASS
+       TODO: eventually make a POST so that we get NAME and PASS at the same time
+    sdk_wifi_set_opmode(STATION_MODE);
+    sdk_wifi_station_set_config(&config);*/
+    // TODO: revisit the idea of a WiFi service/abstractor but since a "true" service
+    // is so complicated, treat it more as a low level framework abstraction 
+    // (consider actually placing it IN frab)
+    // see https://github.com/ourairquality/esp-open-rtos/blob/ourairquality/examples/mqtt_client/mqtt_client.c
+    // for bits and pieces needed to hop onto wifi.  Follow their lead and make a separate RTOS task
+    // NOTE: theirs uses printf with only 256 stack space.  seems dangerous!
 
     return coap_make_response(inpkt->hdr.id, &inpkt->tok,
                               COAP_TYPE_ACK, COAP_RSPCODE_CHANGED,
